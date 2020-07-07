3ec050605e47e55a2552b43cb13cf81cc608fd15
Pierre Narvor <pierre.narvor@ensta-bretagne.fr>
2020-07-07 15:50:53 +0200

    Made a shared library instead of an interace only to provide helper classes and build

---
+++ include/optix_helpers/NVRTC_Helper.h
@@ -0,0 +1,13 @@
+#ifndef _DEF_OPTIX_HELPERS_NVRTC_H_
+#define _DEF_OPTIX_HELPERS_NVRTC_H_
+
+namespace optix {
+
+class NVRTC_Helper
+{
+    int i;
+};
+
+}; //namespace optix
+
+#endif //_DEF_OPTIX_HELPERS_NVRTC_H_
---
+++ src/NVRTC_Helper.cpp
@@ -0,0 +1,8 @@
+#include <optix_helpers/NVRTC_Helper.h>
+
+namespace optix {
+
+int i;
+
+}; //namespace optix
+
