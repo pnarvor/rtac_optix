f3ba060eed7a613ab5fa32dd1b0e51a7019a8906
Pierre Narvor <pnarvor@gmail.com>
2020-08-28 16:30:18 +0200

    Sketch of a GeometryObj

---
+++ include/optix_helpers/Geometry.h
@@ -0,0 +1,44 @@
+#ifndef _DEF_DEFINE_OPTIX_HELPERS_GEOMETRY_H_
+#define _DEF_DEFINE_OPTIX_HELPERS_GEOMETRY_H_
+
+#include <iostream>
+
+#include <optixu/optixpp.h>
+
+#include <optix_helpers/Handle.h>
+#include <optix_helpers/Program.h>
+
+namespace optix_helpers {
+
+class GeometryObj
+{
+    protected:
+    
+    optix::Geometry geometry_;
+    Program intersectionProgram_;
+    Program boundingboxProgram_;
+
+    public:
+
+    GeometryObj(const optix::Geometry& geometry,
+                const Program& intersectionProgram = Program(),
+                const Program& boundingboxProgram = Program());
+
+    optix::Geometry geometry()     const;
+    Program intersection_program() const;
+    Program boundingbox_program()  const;
+};
+
+class Geometry : public Handle<GeometryObj>
+{
+    public:
+
+    Geometry();
+    Geometry(const optix::Geometry& geometry,
+             const Program& intersectionProgram = Program(),
+             const Program& boundingboxProgram = Program());
+};
+
+};
+
+#endif //_DEF_DEFINE_OPTIX_HELPERS_GEOMETRY_H_
---
+++ src/Geometry.cpp
@@ -0,0 +1,40 @@
+#include <optix_helpers/Geometry.h>
+
+namespace optix_helpers {
+
+GeometryObj::GeometryObj(const optix::Geometry& geometry,
+                         const Program& intersectionProgram,
+                         const Program& boundingboxProgram) :
+    geometry_(geometry),
+    intersectionProgram_(intersectionProgram),
+    boundingboxProgram_(boundingboxProgram)
+{
+}
+
+optix::Geometry GeometryObj::geometry() const
+{
+    return geometry_;
+}
+
+Program GeometryObj::intersection_program() const
+{
+    return intersectionProgram_;
+}
+
+Program GeometryObj::boundingbox_program() const
+{
+    return boundingboxProgram_;
+}
+
+Geometry::Geometry() :
+    Handle<GeometryObj>()
+{}
+
+Geometry::Geometry(const optix::Geometry& geometry,
+                   const Program& intersectionProgram,
+                   const Program& boundingboxProgram) :
+    Handle<GeometryObj>(new GeometryObj(geometry, intersectionProgram, boundingboxProgram))
+{}
+
+
+}; //namespace optix_helpers
