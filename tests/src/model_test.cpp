c58f6a2b52600b625cf5daa2f31d782642e34a31
Pierre Narvor <pierre.narvor@ensta-bretagne.fr>
2020-08-31 14:49:25 +0200

    Added sketch of a test for Model type

---
+++ tests/src/model_test.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+#include <optix_helpers/Context.h>
+#include <optix_helpers/RayType.h>
+#include <optix_helpers/Material.h>
+using namespace optix_helpers;
+
+#include "cusamples.h"
+
+int main()
+{
+    Context context;
+
+    RayType rayType0 = context->create_raytype(Source(cusample::coloredRay, "colored_ray.h"));
+    cout << rayType0 << endl;
+
+    Material white0(context->create_material());
+    white0->add_closest_hit_program(rayType0,
+        context->create_program(Source(cusample::whiteMaterial, "closest_hit_white"),
+                                {rayType0->definition()}));
+    cout << white0->get_closest_hit_program(rayType0) << endl;
+
+    Model shere0 = context->create_model();
+
+    return 0;
+}
+
