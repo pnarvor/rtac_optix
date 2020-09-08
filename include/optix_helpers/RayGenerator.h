4711e53c0267af0e7a70f87075c66ac37a93b690
Pierre Narvor <pierre.narvor@ensta-bretagne.fr>
2020-09-08 16:16:30 +0200

    Updated RayGeneratorObj as base type for other generators

--- include/optix_helpers/RayGenerator.h
+++ include/optix_helpers/RayGenerator.h
@@ -2,11 +2,13 @@
 #define _DEF_OPTIX_HELPERS_RAY_GENERATOR_H_
 
 #include <iostream>
-#include <initializer_list>
 
 #include <optixu/optixpp.h>
 
+#include <rtac_base/types/Pose.h>
+
 #include <optix_helpers/Handle.h>
+#include <optix_helpers/Buffer.h>
 #include <optix_helpers/Program.h>
 
 namespace optix_helpers {
@@ -15,46 +17,22 @@ class RayGeneratorObj
 {
     public:
 
-    class Shape
-    {
-        public:
-
-        size_t width;
-        size_t height;
-        size_t depth;
-
-        public:
-
-        Shape(size_t w = 1, size_t h = 1, size_t d = 1);
-        Shape(const std::initializer_list<size_t>& dims);
-
-        size_t size() const;
-    };
+    using Pose = rtac::types::Pose<float>;
 
     protected:
     
-    Shape                shape_;
-    optix::Buffer        renderBuffer_; //buffer where the image will be rendered.
-    RayGenerationProgram raygenProgram_;
-    Program              missProgram_;
-
-    void update_buffer_size();
-    void update_buffer_variable();
+    Buffer  renderBuffer_;  // buffer where the image will be renderered.
+    Program raygenProgram_; // compiled ray generation program.
 
     public:
 
-    RayGeneratorObj(size_t width, size_t height, size_t depth,
-                    const optix::Buffer& buffer,
-                    const RayGenerationProgram& raygen = RayGenerationProgram(),
-                    const Program& miss = Program());
+    RayGeneratorObj(const Buffer& renderBuffer,
+                    const Program& raygenProgram);
 
-    void set_raygen_program(const RayGenerationProgram& program);
-    void set_miss_program(const Program& program);
+    virtual void set_pose(const Pose& pose);
 
-    optix::Buffer render_buffer() const;
-    RayGenerationProgram raygen_program() const;
-    Program miss_program() const;
-    Shape shape() const;
+    Buffer  render_buffer() const;
+    Program raygen_program() const;
 };
 
 class RayGenerator : public Handle<RayGeneratorObj>
@@ -62,10 +40,8 @@ class RayGenerator : public Handle<RayGeneratorObj>
     public:
 
     RayGenerator();
-    RayGenerator(size_t width, size_t height, size_t depth,
-                 const optix::Buffer& buffer,
-                 const RayGenerationProgram& raygen = RayGenerationProgram(),
-                 const Program& miss = Program());
+    RayGenerator(const Buffer& renderBuffer,
+                 const Program& raygenProgram);
 };
 
 
--- src/RayGenerator.cpp
+++ src/RayGenerator.cpp
@@ -2,114 +2,34 @@
 
 namespace optix_helpers {
 
-RayGeneratorObj::Shape::Shape(size_t w, size_t h, size_t d) :
-    width(w), height(h), depth(d)
+RayGeneratorObj::RayGeneratorObj(const Buffer& renderBuffer,
+                                 const Program& raygenProgram) :
+    renderBuffer_(renderBuffer),
+    raygenProgram_(raygenProgram)
+{
+    raygenProgram_->set_buffer(renderBuffer);
+}
+
+void RayGeneratorObj::set_pose(const Pose& pose)
 {}
-
-RayGeneratorObj::Shape::Shape(const std::initializer_list<size_t>& dims) :
-    Shape()
-{
-    auto d = dims.begin();
-    if(d == dims.end()) 
-        return;
-    width = *d;
-    d++;
-    if(d == dims.end()) 
-        return;
-    height = *d;
-    d++;
-    if(d == dims.end()) 
-        return;
-    depth = *d;
-}
-
-size_t RayGeneratorObj::Shape::size() const
-{
-    return width*height*depth;
-}
-
-RayGeneratorObj::RayGeneratorObj(size_t width, size_t height, size_t depth,
-                                 const optix::Buffer& buffer,
-                                 const RayGenerationProgram& raygen,
-                                 const Program& miss) :
-    shape_(width, height, depth),
-    renderBuffer_(buffer),
-    raygenProgram_(raygen),
-    missProgram_(miss)
-{
-    this->update_buffer_size();
-    this->update_buffer_variable();
-} 
  
-void RayGeneratorObj::update_buffer_size()
-{
-    if(!renderBuffer_) 
-        return;
-    switch(shape_.depth) {
-        case 1:
-            renderBuffer_->setFormat(RT_FORMAT_FLOAT);
-            break;
-        case 2:
-            renderBuffer_->setFormat(RT_FORMAT_FLOAT2);
-            break;
-        case 3:
-            renderBuffer_->setFormat(RT_FORMAT_FLOAT3);
-            break;
-        case 4:
-            renderBuffer_->setFormat(RT_FORMAT_FLOAT4);
-            break;
-        default:
-            throw std::runtime_error("Invalid buffer depth (should be [1-4])");
-    }
-    renderBuffer_->setSize(shape_.width, shape_.height);
-}
-
-void RayGeneratorObj::update_buffer_variable()
-{
-    if(raygenProgram_)
-        raygenProgram_->set_render_buffer(renderBuffer_);
-}
-
-void RayGeneratorObj::set_raygen_program(const RayGenerationProgram& program)
-{
-    raygenProgram_ = program;
-    this->update_buffer_variable();
-}
-
-void RayGeneratorObj::set_miss_program(const Program& program)
-{
-    missProgram_ = program;
-}
-
-optix::Buffer RayGeneratorObj::render_buffer() const
+Buffer RayGeneratorObj::render_buffer() const
 {
     return renderBuffer_;
 }
 
-RayGenerationProgram RayGeneratorObj::raygen_program() const
+Program RayGeneratorObj::raygen_program() const
 {
     return raygenProgram_;
-}
-
-Program RayGeneratorObj::miss_program() const
-{
-    return missProgram_;
-}
-
-RayGeneratorObj::Shape RayGeneratorObj::shape() const
-{
-    return shape_;
 }
 
 RayGenerator::RayGenerator() :
     Handle<RayGeneratorObj>()
 {}
 
-RayGenerator::RayGenerator(size_t width, size_t height, size_t depth,
-                           const optix::Buffer& buffer,
-                           const RayGenerationProgram& raygen,
-                           const Program& miss) :
-    Handle<RayGeneratorObj>(new RayGeneratorObj(width, height, depth, buffer, raygen, miss))
+RayGenerator::RayGenerator(const Buffer& renderBuffer,
+                           const Program& raygenProgram) :
+    Handle<RayGeneratorObj>(new RayGeneratorObj(renderBuffer, raygenProgram))
 {}
 
 };
