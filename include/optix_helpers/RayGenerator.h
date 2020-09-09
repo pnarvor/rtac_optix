d2ac18a3ac2e08f0c48b48c4882d7d81d899f8e1
Pierre Narvor <pierre.narvor@ensta-bretagne.fr>
2020-09-09 17:53:48 +0200

    Added some ray geometry in RayGenerator

--- include/optix_helpers/RayGenerator.h
+++ include/optix_helpers/RayGenerator.h
@@ -18,30 +18,48 @@ class RayGeneratorObj
     public:
 
     using Pose = rtac::types::Pose<float>;
+    using Vector3 = rtac::types::Vector3<float>;
+    using Matrix3 = rtac::types::Matrix3<float>;
 
     protected:
     
     Buffer  renderBuffer_;  // buffer where the image will be renderered.
     Program raygenProgram_; // compiled ray generation program.
+    Pose    pose_;
 
     public:
 
     RayGeneratorObj(const Buffer& renderBuffer,
-                    const Program& raygenProgram);
-
+                    const Program& raygenProgram,
+                    const Pose& pose = Pose());
+    
+    // virtual member function to be reimplemented by subclassing.
     virtual void set_pose(const Pose& pose);
+    virtual void set_size(size_t width, size_t height);
+    virtual void set_range(float zNear, float zFar);
 
-    Buffer  render_buffer() const;
+    void look_at(const Vector3& target, const Vector3& up = {0.0,0.0,1.0});
+    void look_at(const Vector3& target,
+                 const Vector3& position,
+                 const Vector3& up = {0.0,0.0,1.0});
+
+    Buffer  render_buffer()  const;
     Program raygen_program() const;
+    Pose    pose()           const;
 };
 
 class RayGenerator : public Handle<RayGeneratorObj>
 {
     public:
 
+    using Pose = RayGeneratorObj::Pose;
+    using Vector3 = RayGeneratorObj::Vector3;
+    using Matrix3 = RayGeneratorObj::Matrix3;
+
     RayGenerator();
     RayGenerator(const Buffer& renderBuffer,
-                 const Program& raygenProgram);
+                 const Program& raygenProgram,
+                 const Pose& pose = Pose());
 };
 
 
--- src/RayGenerator.cpp
+++ src/RayGenerator.cpp
@@ -1,17 +1,73 @@
 #include <optix_helpers/RayGenerator.h>
 
+#include <rtac_base/algorithm.h>
+
 namespace optix_helpers {
 
 RayGeneratorObj::RayGeneratorObj(const Buffer& renderBuffer,
-                                 const Program& raygenProgram) :
+                                 const Program& raygenProgram,
+                                 const Pose& pose) :
     renderBuffer_(renderBuffer),
-    raygenProgram_(raygenProgram)
+    raygenProgram_(raygenProgram),
+    pose_(pose)
 {
     raygenProgram_->set_buffer(renderBuffer);
 }
 
 void RayGeneratorObj::set_pose(const Pose& pose)
-{}
+{
+    pose_ = pose;
+}
+ 
+void RayGeneratorObj::set_size(size_t width, size_t height)
+{
+    (*renderBuffer_)->setSize(width, height);
+}
+
+void RayGeneratorObj::set_range(float zNear, float zFar)
+{
+    //range modifier
+}
+
+void RayGeneratorObj::look_at(const Vector3& target, const Vector3& up)
+{
+    this->look_at(target, pose_.translation(), up);
+}
+
+void RayGeneratorObj::look_at(const Vector3& target,
+                              const Vector3& position,
+                              const Vector3& up)
+{
+    using namespace rtac::types::indexing;
+    // local y points towards target.
+    Vector3 y = target - position;
+    if(y.norm() < 1e-6) {
+        // Camera too close to target, look towards world y.
+        y = Vector3({0.0,1.0,0.0});
+    }
+    y.normalize();
+
+    Vector3 x = y.cross(up);
+    if(x.norm() < 1e-6) {
+        // We are looking towards up. Using last image up direction.
+        x = y.cross(pose_.rotation_matrix()(all,1));
+        if(x.norm() < 1e-6) {
+            // No luck... We have to find another non-colinear vector.
+            x = rtac::algorithm::find_orthogonal(y);
+        }
+    }
+    x.normalize();
+    Vector3 z = x.cross(y);
+
+    rtac::types::Matrix3<float> r;
+    r(all,0) = x; r(all,1) = y; r(all,2) = z;
+
+    this->set_pose(Pose(position, r));
+
+    using namespace std;
+    //cout << "Mat  :\n" << r << endl;
+    cout << "Pose :\n" << pose_.rotation_matrix() << endl;
+}
  
 Buffer RayGeneratorObj::render_buffer() const
 {
@@ -23,13 +79,19 @@ Program RayGeneratorObj::raygen_program() const
     return raygenProgram_;
 }
 
+RayGeneratorObj::Pose RayGeneratorObj::pose() const
+{
+    return pose_;
+}
+
 RayGenerator::RayGenerator() :
     Handle<RayGeneratorObj>()
 {}
 
 RayGenerator::RayGenerator(const Buffer& renderBuffer,
-                           const Program& raygenProgram) :
-    Handle<RayGeneratorObj>(new RayGeneratorObj(renderBuffer, raygenProgram))
+                           const Program& raygenProgram,
+                           const Pose& pose) :
+    Handle<RayGeneratorObj>(new RayGeneratorObj(renderBuffer, raygenProgram, pose))
 {}
 
 };
