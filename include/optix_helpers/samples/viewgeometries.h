34b26edd7bebe4188fd2d5c192f4048cbc291c85
Pierre Narvor <pierre.narvor@ensta-bretagne.fr>
2020-09-24 12:15:57 +0200

    [samples] Removed OrthoView (too much maintain for low use)

--- include/optix_helpers/samples/viewgeometries.h
+++ include/optix_helpers/samples/viewgeometries.h
@@ -1,7 +1,6 @@
 #ifndef _DEF_OPTIX_HELPERS_SAMPLES_VIEW_GEOMETRIES_H_
 #define _DEF_OPTIX_HELPERS_SAMPLES_VIEW_GEOMETRIES_H_
 
-#include <optix_helpers/samples/OrthoView.h>
 #include <optix_helpers/samples/PinHoleView.h>
 
 #endif //_DEF_OPTIX_HELPERS_SAMPLES_VIEW_GEOMETRIES_H_
