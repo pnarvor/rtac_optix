7a35b597506d8eb91fd3cbd7948b7daf67ef4a36
Pierre Narvor <pierre.narvor@ensta-bretagne.fr>
2020-09-24 14:55:31 +0200

    Made Context only able to instanciate RayType

--- include/optix_helpers/RayType.h
+++ include/optix_helpers/RayType.h
@@ -10,38 +10,51 @@
 
 namespace optix_helpers {
 
+// forward declaration for allowing RayType to access RayTypeObj constructor
+class RayType;
+
 class RayTypeObj
 {
     public:
 
     using Index = unsigned int;
     static const Index uninitialized = UINT_MAX;
+    friend class RayType; // To allow RayType to instanciate a RayTypeObj
 
     protected:
 
     Index  rayTypeIndex_; //Index of ray type as defined in optix kernels.
     Source definition_;   //Optix header defining the ray payload type.
-
-    public:
+    
+    private:
 
     RayTypeObj(Index rayTypeIndex, const Source& definition);
+
+    public:
 
     Source definition() const;
     Index    index()    const;
     operator Index()    const;
+
 };
 
+// forward declaration for allowing only ContextObj to build RayTypes.
+class ContextObj;
 class RayType : public Handle<RayTypeObj>
 {
     public:
     
     using Index = RayTypeObj::Index;
     static const Index uninitialized = RayTypeObj::uninitialized;
+    friend class ContextObj;
 
     RayType();
-    RayType(Index rayTypeIndex, const Source& definition);
 
     operator Index() const;
+    
+    private:
+
+    RayType(Index rayTypeIndex, const Source& definition);
 };
 
 }; //namespace optix_helpers
