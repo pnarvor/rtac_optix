0a024e81b92811679d29035bbfb65f7fbe24ecf9
Pierre Narvor <pierre.narvor@ensta-bretagne.fr>
2021-02-05 13:53:21 +0100

    [optix-7/utils] Added a helper to create 0 initialized struct and a template for SBT records

--- optix-7/include/rtac_optix/utils.h
+++ optix-7/include/rtac_optix/utils.h
@@ -2,8 +2,10 @@
 #define _DEF_RTAC_OPTIX_UTILS_H_
 
 #include <iostream>
+#include <cstring>
 #include <sstream>
 
+#include <cuda_runtime.h>
 #include <optix.h>
 
 #define OPTIX_CHECK( call )                                             \
@@ -17,5 +19,28 @@
             throw std::runtime_error(oss.str());                        \
         }                                                               \
     } while(0)                                                          \
+
+
+namespace rtac { namespace optix {
+
+template <typename T>
+T zero()
+{
+    // Simple helper to ensure a T struct is initialized to 0.
+    // (helpful in an initializer list).
+    T res;
+    std::memset(&res, 0, sizeof(T));
+    return res;
+}
+
+template <typename T>
+struct SbtRecord
+{
+    __align__( OPTIX_SBT_RECORD_ALIGNMENT ) char header[OPTIX_SBT_RECORD_HEADER_SIZE];
+    T data;
+};
+
+}; //namespace optix
+}; //namespace rtac
 
 #endif //_DEF_RTAC_OPTIX_UTILS_H_
