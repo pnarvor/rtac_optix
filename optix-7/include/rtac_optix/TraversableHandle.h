81aac5b3bd1b2e2b38b4460222e619b9daf59fde
Pierre Narvor <pierre.narvor@ensta-bretagne.fr>
2021-05-26 16:52:01 +0200

    [optix-7][docs] Documented TransversableHandle

--- optix-7/include/rtac_optix/TraversableHandle.h
+++ optix-7/include/rtac_optix/TraversableHandle.h
@@ -12,6 +12,31 @@
 
 namespace rtac { namespace optix {
 
+/**
+ * **TraversableHandle IS NOT USED ANYMORE BY rtac_optix AND IS TO BE DELETED.**
+ *
+ * An abstract class which represents an element of the object tree.
+ *
+ * In OptiX the ray must interact with objects in the rendering scene.  These
+ * objects can be either physical objects with a geometry and materials
+ * (ObjectInstance), or compound objects which contains other objects
+ * (GroupInstance). These objects form an object tree with a single root,
+ * ObjectInstance being the leaves of the tree and GroupInstance being the
+ * nodes. The root of the object tree is usually given as parameter to the
+ * [optixTrace](https://raytracing-docs.nvidia.com/optix7/api/html/group__optix__device__api.html#gab2bdbb55a09ffbaa1643f1d7e2fcfcc9)
+ * function as an
+ * [OptixTraversableHandle](https://raytracing-docs.nvidia.com/optix7/api/html/group__optix__types.html#gaacf20eb67c33c2c1849adc058d43cff7).
+ *
+ * Both ObjectInstance and GroupInstance are high-level easier to use wrappers
+ * around more low-level OptiX objects. The TraversableHandle, rtac_optix
+ * counterpart of
+ * [OptixTraversableHandle](https://raytracing-docs.nvidia.com/optix7/api/html/group__optix__types.html#gaacf20eb67c33c2c1849adc058d43cff7),
+ * is the lowest common denominator between the ObjectInstance and the
+ * GroupInstance classes and represent nothing more than an element of the
+ * object tree. It is implicitly castable to an
+ * [OptixTraversableHandle](https://raytracing-docs.nvidia.com/optix7/api/html/group__optix__types.html#gaacf20eb67c33c2c1849adc058d43cff7)
+ * but specializations of TraversableHandles must implement the conversion.
+ */
 struct TraversableHandle
 {
     // ABSTRACT class
@@ -20,16 +45,19 @@ struct TraversableHandle
     // rendering tree. The main purpose of this type is to be able to infer the
     // sbt records positions by exploring the world tree.
 
-    // This class represent a node of the scene graph in which the ray will
-    // propagate. OptixTraversableHandle is a generic type which can represent
-    // any item in the graph (geometries, transforms, "structural" nodes
-    // containing other nodes, etc...)
-    
     public:
 
     using Ptr      = Handle<TraversableHandle>;
     using ConstPtr = Handle<const TraversableHandle>;
-    
+   
+    /**
+     * Implicit cast to
+     * [OptixTraversableHandle](https://raytracing-docs.nvidia.com/optix7/api/html/group__optix__types.html#gaacf20eb67c33c2c1849adc058d43cff7).
+     * To be reimplemented in subclasses.
+     *
+     * @return a valid
+     * [OptixTraversableHandle](https://raytracing-docs.nvidia.com/optix7/api/html/group__optix__types.html#gaacf20eb67c33c2c1849adc058d43cff7).
+     */
     virtual operator OptixTraversableHandle() = 0;
 
     // This represent the width in sbt offsets that the 
